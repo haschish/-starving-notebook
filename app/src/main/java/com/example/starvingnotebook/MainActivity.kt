88b30e0f31534a901e30691ca7bf8dd5f264be6d
Lenkeri <lno4kamalt@yandex.ru>
2022-04-15 18:03:09 +0500

    fghf

--- app/src/main/java/com/example/starvingnotebook/MainActivity.kt
+++ app/src/main/java/com/example/starvingnotebook/MainActivity.kt
@@ -7,25 +7,30 @@ import android.widget.Button
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.setupButton).setOnClickListener {
-            val navHostFragment = findViewById<View>(R.id.navHost)
-            navHostFragment.findNavController().navigate(R.id.action_notesFragment_to_setupFragment)
-        }
-        findViewById<Button>(R.id.statButton).setOnClickListener {
-            val navHostFragment = findViewById<View>(R.id.navHost)
-            navHostFragment.findNavController().navigate(R.id.action_setupFragment_to_statisticsFragment2)
+        val navController = findViewById<View>(R.id.navHost).findNavController()
+        findViewById<BottomNavigationView>(R.id.buttonsNav).setupWithNavController(navController)
 
-        }
-        findViewById<Button>(R.id.notesButton).setOnClickListener {
-            val navHostFragment = findViewById<View>(R.id.navHost)
-            navHostFragment.findNavController().navigate(R.id.action_statisticsFragment2_to_notesFragment)
-
-        }
+//        findViewById<Button>(R.id.setupButton).setOnClickListener {
+//            val navHostFragment = findViewById<View>(R.id.navHost)
+//            navHostFragment.findNavController().navigate(R.id.action_notesFragment_to_setupFragment)
+//        }
+//        findViewById<Button>(R.id.statButton).setOnClickListener {
+//            val navHostFragment = findViewById<View>(R.id.navHost)
+//            navHostFragment.findNavController().navigate(R.id.action_setupFragment_to_statisticsFragment2)
+//
+//        }
+//        findViewById<Button>(R.id.notesButton).setOnClickListener {
+//            val navHostFragment = findViewById<View>(R.id.navHost)
+//            navHostFragment.findNavController().navigate(R.id.action_statisticsFragment2_to_notesFragment)
+//
+//        }
     }
 }
